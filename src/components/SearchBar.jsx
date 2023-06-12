478f2b0cb0c4fc6d31c7f1700ec562366d5782a3
eobcre <eobcre@gmail.com>
2023-06-11 23:49:24 -0400

    label tag removed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,12 +4,10 @@ const SearchBar = () => {
       <form className='app-form'>
         <h2 className='app-title'>Movie Search App</h2>
         <div>
-          <label>
-            <input type='text' className='app-input' />
-            <button type='submit' className='app-button'>
-              Search
-            </button>
-          </label>
+          <input type='text' className='app-input' />
+          <button type='submit' className='app-button'>
+            Search
+          </button>
         </div>
       </form>
     </div>
