56151ea932c2bd93a03ee96c447d51386df0d8aa
eobcre <eobcre@gmail.com>
2023-06-11 23:12:40 -0400

    new component created

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+const SearchBar = () => {
+  return (
+    <div className='app-container'>
+      <h2>Movie Search App</h2>
+      <div>
+        <label>
+          <input type='text' />
+          <button>Search</button>
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
