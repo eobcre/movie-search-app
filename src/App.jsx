7a4c0db752c001a36a70a192e10fb5be7bdb0147
eobcre <eobcre@gmail.com>
2023-06-11 22:50:18 -0400

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Footer from './components/Footer';
+
 const App = () => {
   return (
     <div>
       <h2>Movie Search App</h2>
       <input type='text' />
       <button>Search</button>
+      <Footer />
     </div>
   );
 };
