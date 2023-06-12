ef68065f8bb91918df865f9143426daf8e7c63e6
eobcre <eobcre@gmail.com>
2023-06-12 10:12:43 -0400

    submit function logic

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,13 +2,20 @@ import Logo from '../assets/logo.png';
 import { BiSearch } from 'react-icons/Bi';
 
 const SearchBar = () => {
+  const getMovies = async (e) => {
+    e.preventDefault();
+    console.log('Submit');
+  };
+
   return (
     <div className='app-container'>
-      <form className='app-form'>
+      <form onSubmit={getMovies} className='app-form'>
         <img src={Logo} alt='Logo' width='100' className='app-img' />
         <div className='app-search'>
           <input type='text' className='app-input' />
-          <BiSearch className='app-icon' />
+          <button type='submit' className='app-button'>
+            <BiSearch className='app-icon' />
+          </button>
         </div>
       </form>
     </div>
