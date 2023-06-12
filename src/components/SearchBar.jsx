a87944a631ed5d07628ab241819f2e76d8a25821
eobcre <eobcre@gmail.com>
2023-06-12 09:21:46 -0400

    className app-img, app-search and app-icon added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,14 @@
 import Logo from '../assets/logo.png';
+import { BiSearch } from 'react-icons/Bi';
 
 const SearchBar = () => {
   return (
     <div className='app-container'>
       <form className='app-form'>
-        <img src={Logo} alt='Logo' width='100' />
-        <div>
+        <img src={Logo} alt='Logo' width='100' className='app-img' />
+        <div className='app-search'>
           <input type='text' className='app-input' />
-          <button type='submit' className='app-button'>
-            Search
-          </button>
+          <BiSearch className='app-icon' />
         </div>
       </form>
     </div>
