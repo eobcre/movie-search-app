bd30eeed9cf537dc05abd3e6ea21c4886fdd91a8
eobcre <eobcre@gmail.com>
2023-06-11 23:46:34 -0400

    app-container class added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,18 @@
 const SearchBar = () => {
   return (
-    <form className='app-form'>
-      <h2 className='app-title'>Movie Search App</h2>
-      <div>
-        <label>
-          <input type='text' className='app-input' />
-          <button type='submit' className='app-button'>
-            Search
-          </button>
-        </label>
-      </div>
-    </form>
+    <div className='app-container'>
+      <form className='app-form'>
+        <h2 className='app-title'>Movie Search App</h2>
+        <div>
+          <label>
+            <input type='text' className='app-input' />
+            <button type='submit' className='app-button'>
+              Search
+            </button>
+          </label>
+        </div>
+      </form>
+    </div>
   );
 };
 
